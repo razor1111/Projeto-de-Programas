ba8a91e1732cb6f837c3794c2997b0f7cd8ba678
razor1111 <netinho-3003@hotmail.com>
2017-06-28 15:31:26 -0400

    Delete Jogo.java

--- Jogo.java
+++
@@ -1,9 +0,0 @@
-package antoniocalebethiago;
-
-public final class Jogo {
-    public void executaAcoesComuns(Personagem p){
-        p.andar();
-        p.guardarItem();
-        p.usarItem();
-    }
-}
